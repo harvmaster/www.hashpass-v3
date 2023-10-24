f427e8b6588f242ae1b077c9576942ea9553c112
Harvmaster <hzuccon@gmail.com>
2023-10-24 12:44:08 +1100

    Removed debug code from service store

--- src/stores/service.ts
+++ src/stores/service.ts
@@ -47,17 +47,6 @@ export const useServiceStore = defineStore('service', {
       const services = Services as Service[]
       this.services = services
 
-      const temp = this.services.map(service => {
-        service.note = {
-          username: 'testUsername',
-          email: 'test@email.com',
-          other: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum voluptates.'
-        }
-        return {...service}
-      })
-
-      console.log(temp)
-
       return;
 
       // Load the services from the local storage
